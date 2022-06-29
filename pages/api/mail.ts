69933c94385c9d00f3e78286b83a21ae6b25d7a7
Rogibb111 <Rogibb111@users.noreply.github.com>
2022-06-29 07:20:53 -0600

    Adding Functionality For Getting Labels

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,8 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { promises } from 'fs'
-import { TOKEN_FLAG, TOKEN_PATH } from '../common/literals';
+import { Labels, LabelsList, TOKEN_FLAG, TOKEN_PATH } from '../common/literals';
 import { getOauth2Client } from '../common/functions';
+import { OAuth2Client } from 'google-auth-library';
+import { gmail_v1, google } from 'googleapis';
+import { TokenError } from '../common/errors';
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,16 +13,54 @@ export default async function handler(
 ) {
     const oAuth2Client = getOauth2Client(res);  
     try {
-      const token = await promises.readFile(TOKEN_PATH);
-      oAuth2Client.setCredentials(JSON.parse(token.toString()));
+      await getToken(oAuth2Client);
+      await getMail(oAuth2Client);
       res.status(200).send('Success');
     } catch (err) {
-      const authUrl = oAuth2Client.generateAuthUrl({
-        access_type: 'offline',
-        scope: ['https://www.googleapis.com/auth/gmail.readonly'],
-      });
-      process.env[TOKEN_FLAG] = authUrl;
-      console.log('No Token Available. Need to Authenticate with Google to get Token file. Err: '+err);
-      res.status(500).send('Failed. No Token Available.');
+      if (err instanceof TokenError) {
+        getAuthUrl(oAuth2Client, err);
+      } else {
+        console.log(err);
+      }
+      res.status(500).send('Failed. Error getting mail from Gmail.');
     }
+}
+
+async function getToken(oAuth2Client: OAuth2Client) {
+  try {
+    const token = await promises.readFile(TOKEN_PATH);
+    oAuth2Client.setCredentials(JSON.parse(token.toString()));
+  } catch (err) {
+    throw new TokenError(err as string);
+  }
+}
+
+function getAuthUrl(oAuth2Client: OAuth2Client, err: unknown) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: ['https://www.googleapis.com/auth/gmail.readonly'],
+  });
+  process.env[TOKEN_FLAG] = authUrl;
+  console.log('No Token Available. Need to Authenticate with Google to get Token file. Err: '+err);
+}
+
+async function getMail(auth: OAuth2Client) {
+  const gmail = google.gmail({version: 'v1', auth});
+  const labelMap = getLabels(gmail);
+  // TODO: Fill out rest of getting mail with right lables
+
+}
+
+async function getLabels(gmail: gmail_v1.Gmail): Promise<Map<String, String>> {
+  const labelMap = new Map();
+  const { data } = await gmail.users.labels.list({ userId: 'me' });
+  if (!data.labels) {
+    throw new Error('No Labels in Gmail account! Gmail (or Google API) is screwed up!');
+  }
+  for(let label of data.labels) {
+    if(label && LabelsList.includes(label.name as Labels)) {
+      labelMap.set(label.name, label.id);
+    }
+  }
+  return labelMap;
 }
--- pages/common/literals.ts
+++ pages/common/literals.ts
@@ -1,6 +1,15 @@
 export const TOKEN_PATH = 'token.json';
 export const TOKEN_FLAG = 'AUTH_ROUTE';
 
+export enum Labels {
+    Location = 'Zoleo/Location',
+    Messages = 'Zoleo/Messages'
+}
+
+export const LabelsList = Object.values(Labels);
+
 export default {
-    tokenPath: TOKEN_PATH
+    tokenPath: TOKEN_PATH,
+    tokenFlag: TOKEN_FLAG,
+    labels: Labels
 }
