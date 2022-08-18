b3fd55c7229bfb783c091013406f0ca71a599147
Robert Gibb <rogibb111@gmail.com>
2022-08-18 20:15:33 +0000

    Fixing Missed Name Change

--- pages/trips/[trip].tsx
+++ pages/trips/[trip].tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import { AppProps } from '../../common/types'
 import { getLocations, getMessages, getTrips } from '../../common/functions'
 import Main from '../../components/main'
-import { CURRENT_TRIP } from '../../common/literals'
+import { DEFAULT_TRIP } from '../../common/literals'
 
 
 const Trips: NextPage<AppProps> = ({ tokenURL, apiKey, locations, messages, trips, page }) => {
@@ -24,8 +24,8 @@ export async function getServerSideProps(context: any) {
       page: trip.toUpperCase(),
       tokenURL:process.env.AUTH_ROUTE || null,
       apiKey: process.env.GOOGLE_MAPS_API_KEY || '',
-      locations: await getLocations(trip || CURRENT_TRIP),
-      messages: await getMessages(trip || CURRENT_TRIP),
+      locations: await getLocations(trip || DEFAULT_TRIP),
+      messages: await getMessages(trip || DEFAULT_TRIP),
       trips: await getTrips()
     }
   };
