f50bef21b4a4eb21c4232989f883a6f009879837
Robert Gibb <Rogibb111@users.noreply.github.com>
2022-09-07 14:42:32 -0600

    More Logging Updates (#4)
    - stringified token for console log
    - Updated Token error logging message
    - Array checking auth data for admin auth screen

--- pages/admin/auth.tsx
+++ pages/admin/auth.tsx
@@ -19,6 +19,11 @@ const AdminAuth: NextPage<AdminAuthProps> = ({ authUrl, authData }) => {
       method: "get",
     });
 
+    if (!Array.isArray(authData)) {
+      console.log('AuthData: ' + authData);
+      authData = [];
+    }
+
     const json = await res.json();
     switch (res.status) {
       case 200:
